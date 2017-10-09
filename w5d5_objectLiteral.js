5c176a568c24747dc29430779b37cb98c35b8162
Riski Nugroho Putra <31657885+riskinputra@users.noreply.github.com>
2017-10-09 10:40:16 +0700

    First Commit

---
+++ w5d5_objectLiteral.js
@@ -0,0 +1,22 @@
+var student = {
+  id: 'A017001',
+  name: 'Suketi',
+  age: 25,
+  bday: '01-08-1993',
+  gender:[
+    'male',
+    'female'
+  ],
+  hobby:[
+    'Ngoding',
+    'Gaming',
+    'Traveling'
+  ]
+};
+
+console.log('Student Id : '+student.id);
+console.log('Name : '+student.name);
+console.log('Age : '+student.age);
+console.log('Date of birth : '+student.bday);
+console.log('Gender : '+student.gender[1]);
+console.log('Hobby : '+student.hobby);
